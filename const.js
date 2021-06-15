7c3b4061b64da9fa16d58bd4307954d7c90f5db2
Pavel <pavel.priladyshev@gmail.com>
2021-06-15 15:09:48 +0300

    init: begin task

---
+++ const.js
@@ -0,0 +1,14 @@
+const $arenas = document.querySelector(".arenas");
+const $randomButton = document.querySelector(".button");
+const $formFight = document.querySelector(".control");
+const $chat = document.querySelector(".chat");
+
+const HIT = {
+  head: 30,
+  body: 25,
+  foot: 20,
+};
+
+const ATTACK = ["head", "body", "foot"];
+
+export { $arenas, $randomButton, $formFight, $chat, HIT, ATTACK };
